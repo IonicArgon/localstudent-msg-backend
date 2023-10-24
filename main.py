d1922f483ff2bbd5694d7c760a6c7a2936109ac4
Marco Tan <marco.tan.200405@gmail.com>
2023-10-24 16:18:33 -0400

    move all this over to a temp folder while i refactor

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 from flask import Flask
 
-from app.routes.leadsRoute import leads_route_bp
-from app.routes.connectRoute import connect_route_bp
-from app.routes.signinRoute import signin_route_bp
-
 app = Flask(__name__)
-
-# register the blueprints
-app.register_blueprint(leads_route_bp, url_prefix="/api")
-app.register_blueprint(connect_route_bp, url_prefix="/api")
-app.register_blueprint(signin_route_bp, url_prefix="/api")
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=5000, use_reloader=True)
